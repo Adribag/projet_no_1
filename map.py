104fd0f83a548d641f51d0fcdec5ba04081290c5
Adri <adrien.baglin@gmail.com>
2021-10-06 11:47:10 +0200

    first commit

---
+++ map.py
@@ -0,0 +1,218 @@
+map = [
+    # [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,1,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,49],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,49]
+    ]
+
+import os, sys
+
+
+mapIco = "🗺️"
+treeIco = "🏝️"
+waveIco = "🌊"
+borderMap = "🟫"
+sandIco = "🟨"
+waterIco = "🟦"
+florIco = "🟩"
+questIco = "🟪"
+palmTreeIco = "🌴"
+playerIco = "👨"
+
+
+def drawMap(posY,posX,symb):
+    map[posY][posX] = symb
+
+def printBorderMap():
+    for line in range (len(map)):
+        if(line == 0 or line == 29):
+            for i in range(len(map[line])):
+                map[line][i] = borderMap
+        else:
+            map[line][0] = borderMap
+            map[line][49] = borderMap
+
+def printSea():
+    for i in range(1,49):
+        drawMap(28,i,waterIco)
+        drawMap(27,i,waterIco)
+
+def printRiver():
+    for x in range(21,26):
+        for y in range(20,27):
+            drawMap(y,x,waterIco)
+    for x in range(20,24):
+        for y in range(14,20):
+            drawMap(y,x,waterIco)
+    for x in range(19,22):
+        for y in range(8,14):
+            drawMap(y,x,waterIco)
+    for x in range(18,20):
+        for y in range(5,8):
+            drawMap(y,x,waterIco)
+    for x in range(18,19):
+        for y in range(1,5):
+            drawMap(y,x,waterIco)
+
+def printSand():
+    for i in range(1,49):
+        drawMap(26,i,sandIco)
+        drawMap(25,i,sandIco)
+    for i in range(30,49):
+        drawMap(24,i,sandIco)
+    
+def printQuests():
+    # Première quete
+    drawMap(25,27,questIco)
+    # Deuxieme quete
+    drawMap(10,48,questIco)
+    # Troisieme quete
+    drawMap(20,10,questIco)
+    # Porte finale
+    drawMap(1,20,questIco)
+
+def fondMap():
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            map[i][j] = florIco
+
+def affichageMap(map,poY = 0,poX = 0):
+    
+    for poY in range(len(map)):
+        for poX in range(len(map[poY])):
+            if(poX == 0):
+                print()
+            # map[i][j] = florIco
+            
+            # if posY != 0 and posX != 0:              
+            print(map[poY][poX], end ="")
+            
+                # print(f" valeur de remvove = {remove}")
+            # affichePlayer(posY,posX)
+   
+
+quest = "start"
+def moveplayer():
+
+    startPosY = 25
+    startPosX = 45
+    drawMap(startPosY,startPosX,playerIco) 
+    affichageMap(map)
+    # affichePlayer(startPosY,startPosX)
+    currentPos = sandIco
+    while quest != "end":
+
+        moving = input("Vers quelle direction aller : ").upper()
+        print()
+        print(moving) 
+
+        if moving == "Z":  
+            oldIco = map[startPosY-1][startPosX]
+            startPosY -= 1 
+
+            ClearConsole()
+            drawMap(startPosY +1,startPosX,currentPos)  
+            drawMap(startPosY,startPosX,playerIco)
+            affichageMap(map) 
+
+            currentPos = oldIco
+            
+        if moving == "S":
+            oldIco = map[startPosY+1][startPosX]
+            startPosY += 1 
+
+            ClearConsole()
+            drawMap(startPosY -1,startPosX,currentPos)  
+            drawMap(startPosY,startPosX,playerIco)
+            affichageMap(map) 
+              
+            currentPos = oldIco
+
+        if moving == "Q":
+            oldIco = map[startPosY][startPosX-1]
+            startPosX -= 1 
+
+            ClearConsole()
+            drawMap(startPosY,startPosX +1,currentPos)  
+            drawMap(startPosY,startPosX,playerIco)
+            affichageMap(map) 
+              
+            currentPos = oldIco
+
+        if moving == "D":
+
+            oldIco = map[startPosY][startPosX+1]
+            startPosX += 1 
+
+            ClearConsole()
+            drawMap(startPosY,startPosX -1,currentPos)  
+            drawMap(startPosY,startPosX,playerIco)
+            affichageMap(map) 
+              
+            currentPos = oldIco
+        
+        if(startPosY == 25 and startPosX == 27):
+            print()
+            print("Tu es sur la première quete")
+            question = input("Veux tu lancer la quete O -> Oui | N -> Non : ").upper()
+            if(question == "O"):
+                print("Tu lance la quete")
+            else:
+                print("Passe ton chemin")
+        
+        print()
+        print(f"X = {startPosX} | Y = {startPosY}")
+
+def ClearConsole():
+    """
+        Clear the console depending on OS
+    """
+
+    if "win" in sys.platform.lower():
+        # for windows
+        os.system("cls")
+    elif "linux" in sys.platform.lower():
+        # for linux
+        os.system("clear")
+
+
+
+# affichePlayer(posY = 25,posX = 45)
+
+# affichageMap(map)
+fondMap()
+printSea()
+printSand()
+printRiver()
+printQuests()
+printBorderMap()
+moveplayer()
+
+
